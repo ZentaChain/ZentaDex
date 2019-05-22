f632001f80b5adab7fac1fa7148fc7d3a6e1b5d6
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2019-05-22 13:13:20 +0200

    Update 1

--- dex/src/contracts/Token.sol
+++ dex/src/contracts/Token.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract Token {
+
+    using SafeMath for uint;
+
     string public name = "Zenta";
     string public symbol = "TEST";
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
-
     mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
 
     constructor() public {
@@ -15,4 +21,12 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0));
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
