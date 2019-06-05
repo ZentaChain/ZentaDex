24c74a7b6e772bb5b274b501e4c6026946fe8695
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2019-06-05 04:43:41 +0200

    Zentadex

--- dex/src/App.js
+++ dex/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Navbar from './Navbar'
+import Content from './Content'
+import { connect } from 'react-redux'
+import {
+  loadWeb3,
+  loadAccount,
+  loadToken,
+  loadZentadex
+} from '../store/interactions'
+import { contractsLoadedSelector } from '../store/selectors'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData(this.props.dispatch)
+  }
+
+  async loadBlockchainData(dispatch) {
+    const web3 = loadWeb3(dispatch)
+    await web3.eth.net.getNetworkType()
+    const networkId = await web3.eth.net.getId()
+    await loadAccount(web3, dispatch)
+    const token = await loadToken(web3, networkId, dispatch)
+    if(!token) {
+      window.alert('Token Smart-Contract not detected on the current network. Please select another network with Metamask.')
+      return
+    }
+    const zentadex = await loadZentadex(web3, networkId, dispatch)
+    if(!zentadex) {
+      window.alert('Zentadex Smart-Contract not detected on the current network. Please select another network with Metamask.')
+      return
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        { this.props.contractsLoaded ? <Content /> : <div className="content"></div> }
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    contractsLoaded: contractsLoadedSelector(state)
+  }
+}
+
+export default connect(mapStateToProps)(App)
