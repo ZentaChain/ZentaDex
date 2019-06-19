7821fb457aecaa5e2bb2dae481b58ba0ce567373
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2019-06-19 19:49:25 +0200

    Add files via upload

--- dex/src/components/Navbar.js
+++ dex/src/components/Navbar.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { accountSelector } from '../store/selectors'
 
+
+
+
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar .collapse.navbar-collapse">
-          <a class="navbar-brand" href="https://zentadex.com">
-            <img src="" width="30" height="30" class="d-inline-block align-top" alt=""></img>
-                Zentadex
-          </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <nav className="navbar .collapse.navbar-collapse fixed-top">
+        <a class="logo d-inline-block align-top"><a href="http://zentadex.com" rel="ZentadexLogo">ZENTADEX</a></a>
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+           <span className="navbar-toggler-icon"></span>
+         </button>
           <div className="nav-button d-inline-block align-top">
               <button type="button" class="btn btn-sm btn-outline-primary text-light">Home</button>
               <span class="sr-only">(current)</span>
@@ -28,14 +28,15 @@ class Navbar extends Component {
               <button type="button" class="btn btn-sm btn-outline-primary text-light">Contact</button>
               &nbsp;
           </div>
+          
 
-          <a className="nav-metamask btn-sm small" id="navMetamask">
+          <a className="nav-metamask btn-sm small btn btn-outline-success" id="navMetamask">
             <a
               className = "nav-metamask text-light"
                 href={`https://etherscan.io/address/${this.props.account}`}
               target="_blank"
               rel="noopener noreferrer"
-            > Connected:
+            > CONNECTED:
               {this.props.account}
             </a>
           </a>
