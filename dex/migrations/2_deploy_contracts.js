2c2e2571a9cf09a94fb5b38e846bcd22e4d6512c
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2019-06-23 23:20:09 +0200

    Zentadex

--- dex/migrations/2_deploy_contracts.js
+++ dex/migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 const Token = artifacts.require("Token");
+const Zentadex = artifacts.require("Zentadex");
 
-module.exports = function(deployer) {
-  deployer.deploy(Token);
+module.exports = async function(deployer) {
+  const accounts = await web3.eth.getAccounts()
+
+  await deployer.deploy(Token);
+
+  const feeAccount = accounts[0]
+  const feePercent = 0
+
+  await deployer.deploy(Zentadex, feeAccount, feePercent)
 };
