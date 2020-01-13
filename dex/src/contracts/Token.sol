322973435066f9d72c8a2b8ffab89645e7333a24
gokselp <50344148+gokselp@users.noreply.github.com>
2020-01-14 02:20:03 +0300

    Update Token.sol

--- dex/src/contracts/Token.sol
+++ dex/src/contracts/Token.sol
@@ -11,8 +11,6 @@ contract Token {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
-
-
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
@@ -48,4 +46,4 @@ contract Token {
         _transfer(_from, _to, _value);
         return true;
     }
-}
+});
